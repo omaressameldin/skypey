6c97d99f28f64b15eed5a7ebac3e049bf5249bf5
Omar Essam <o.essameldin@gmail.com>
2019-03-15 11:20:47 +0100

    adjust Main Component to use activeUSErId and user and to render Empty component

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React from 'react';
 import styled from 'styled-components';
+import { compose } from 'redux';
+import activeUserIdProvider from 'redux/activeUserId/provider'
+import userProvider from 'redux/user/provider'
+
 import { device } from 'device';
+import Empty from 'components/Empty'
+
+const providers = compose(
+  activeUserIdProvider,
+  userProvider,
+)
 
 const StyledMain = styled.main`
   flex: 1 1 0;
   background-color: rgba(25, 25, 27, 1);
   height: 100%;
 `;
-  const Main = () => (
-    <StyledMain>
-      Main Stuff
-    </StyledMain>
-  )
 
-export default Main;
+const Main = ({ user, activeUserId }) => {
+  const renderMainContent = () => {
+    if (!activeUserId) {
+      return <Empty user={user} />;
+    } else {
+      // return <ChatWindow activeUserId={activeUserId} />;
+      return "Main Stuff";
+    }
+  };
+  return (
+    <StyledMain>
+    {renderMainContent()}
+  </StyledMain>
+  );
+};
+
+export default providers(Main);
