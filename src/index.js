9f5b0dcd47bcdfea0e040e36dc6f3ab34f9d85a4
Omar Essam <o.essameldin@gmail.com>
2019-03-15 10:38:08 +0100

    adjust index of page to include provider and store

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './redux/store'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function fancyLog() {
+  console.log("%c Rendered with 👉 👉👇", "background: purple; color: #FFF");
+}
+
+const render = () => {
+  fancyLog();
+
+  return (ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')));
+}
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
